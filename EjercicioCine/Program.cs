f5ed16a0393a585dbd50f2b2c5ae56aa839cb9b1
DanielAndrade1496 <92600040+DanielAndrade1496@users.noreply.github.com>
2021-11-20 16:57:45 -0500

    creacion peliculas

--- EjercicioCine/Program.cs
+++ EjercicioCine/Program.cs
@@ -4,9 +4,17 @@ namespace EjercicioCine
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // Peliculas
+            Peliculas peli = new Peliculas();
+            peli.Nombre = "John Wick 3";
+            peli.Anio = 2019;
+            peli.Duracion = 131;
+
+
         }
     }
 }
