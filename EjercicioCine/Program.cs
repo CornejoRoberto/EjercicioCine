39c7b87bc231972598f61244a06961ad97bceb08
DanielAndrade1496 <92600040+DanielAndrade1496@users.noreply.github.com>
2021-11-20 17:05:30 -0500

    Menu

--- EjercicioCine/Program.cs
+++ EjercicioCine/Program.cs
@@ -7,12 +7,17 @@ namespace EjercicioCine
        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
             // Peliculas
             Peliculas peli = new Peliculas();
             peli.Nombre = "John Wick 3";
             peli.Anio = 2019;
             peli.Duracion = 131;
+
+            Console.WriteLine("Bienvendios al cine XYZ ");
+            Console.WriteLine("MENU /r");
+            Console.WriteLine("Bienvendios al cine XYZ ");
+            Console.WriteLine("Bienvendios al cine XYZ ");
 
 
         }
