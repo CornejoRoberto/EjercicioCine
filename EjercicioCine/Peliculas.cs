9e49b92f235445691517d61acb181f139d4ae9cb
DanielAndrade1496 <92600040+DanielAndrade1496@users.noreply.github.com>
2021-11-20 16:34:34 -0500

    Atributos Peliculas

--- EjercicioCine/Peliculas.cs
+++ EjercicioCine/Peliculas.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace EjercicioCine
 {
-    class Peliculas
+    class Peliculas:Salas
     {
+        public string Nombre { get; set; }
+        public int Anio { get; set; }
+        public int Duracion { get; set; }
+
     }
 }
